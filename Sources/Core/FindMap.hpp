9ebaf0ad62c2cf9f06d03daf93ed80302c6a2dd0
Handi Dwi Cahyo <87643319+Wisle25@users.noreply.github.com>
2022-12-06 22:37:10 +0700

    Delete Sources directory

--- Sources/Core/FindMap.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-template <typename T>
-std::map<std::string, T> FindMap(std::map<std::string, T>& I_Map, const std::string key);
--- Sources/Core/Vector.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-sf::Vector2f Normalized(sf::Vector2f Vector);
-
-float VectorCross(const sf::Vector2f& V1, const sf::Vector2f& V2);
-
-float VectorDot(const sf::Vector2f& V1, const sf::Vector2f& V2);
-float VectorDot(const sf::Vector3f& V1, const sf::Vector3f& V2);
